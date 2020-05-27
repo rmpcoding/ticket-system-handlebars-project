825a092f574ec1cb0e6e393077f0570644cc1493
Robert Parsons <rmpcoding@gmail.com>
2020-05-27 01:42:10 -0700

    Adds middleware to tickets_controller.js

--- controllers/tickets_controller.js
+++ controllers/tickets_controller.js
@@ -2,8 +2,18 @@ const express = require('express');
 const orm = require('../models/ticket');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  orm.selectAll
+  res.render("index")
+})
 
 app.listen(PORT, (err) => {
   console.log(`Server listening on: http://localhost: ${PORT}`)
