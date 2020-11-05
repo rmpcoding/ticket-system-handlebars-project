5ee739dd9fa00b2860e2ee0e96633bdbcb81af6b
Robert Parsons <rmpcoding@gmail.com>
2020-11-04 22:56:52 -0800

    Removes unnecessary comments

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,6 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// mysql://b2347b8e1182d8:2a19d6dc@us-cdbr-east-02.cleardb.com/heroku_409be8251eed992?reconnect=true
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
