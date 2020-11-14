c75bda41d4b9de392d03bc8ee8a7ebe85693b05c
Robert Parsons <rmpcoding@gmail.com>
2020-11-14 14:49:25 -0800

    Adds connection and removes pool for mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,25 @@
 const mysql = require('mysql');
 require('dotenv').config();
-let pool;
 let connection;
 
 if (process.env.CLEARDB_DATABASE_URL) {
     connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
     connection.connect();
 } else {
-    pool = mysql.createPool({
+    connection = mysql.createConnection({
         connectionLimit: 5,
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PW,
         database: process.env.DB,
     });
-    pool.getConnection(function (err) {
+    connection.connect(function (err) {
         if (err) {
             console.log(`There was an error connecting: ${err.stack}`);
         }
-        console.log(`Connected as id ${pool.threadId}`);
+        console.log(`Connected as id ${connection.threadId}`);
     });
 }
 
 
-module.exports = pool;
+module.exports = connection;
