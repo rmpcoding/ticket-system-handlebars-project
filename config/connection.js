d799608293760880dad137bd42427f69622e559d
Robert Parsons <rmpcoding@gmail.com>
2020-11-13 18:56:13 -0800

    Adds if statement to check whether cleardb exists

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
 require('dotenv').config();
+let pool;
 
+if (process.env.CLEARDB_DATABASE_URL) {
+    pool = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+} else {
+    pool = mysql.createPool({
+        connectionLimit: 5,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB,
+    });
+}
 
-const pool = mysql.createPool({
-    connectionLimit: 5,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB,
-});
 
 pool.getConnection(function (err) {
     if (err) {
